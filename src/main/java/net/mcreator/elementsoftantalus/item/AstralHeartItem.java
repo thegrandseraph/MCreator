038ca3cbded2f696fbe17b1756d812c1cbbc2db8
DomAwesome09 <Domenick@DOMENICKSLAPTOP>
2022-01-30 17:24:29 -0500

    added misc folder

--- src/main/java/net/mcreator/elementsoftantalus/item/AstralHeartItem.java
+++ src/main/java/net/mcreator/elementsoftantalus/item/AstralHeartItem.java
@@ -1,10 +1,19 @@
 
 package net.mcreator.elementsoftantalus.item;
 
-import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.Component;
+
+import net.mcreator.elementsoftantalus.init.ElementsOfTantalusModTabs;
+
+import java.util.List;
 
 public class AstralHeartItem extends Item {
-
 	public AstralHeartItem() {
 		super(new Item.Properties().tab(ElementsOfTantalusModTabs.TAB_ELEMENTS_OF_TANTALUS).stacksTo(64).rarity(Rarity.COMMON));
 		setRegistryName("astral_heart");
@@ -26,5 +35,4 @@ public class AstralHeartItem extends Item {
 		list.add(new TextComponent("Did he ever regret his actions."));
 		list.add(new TextComponent("He will never be able to ascend as you can."));
 	}
-
 }
