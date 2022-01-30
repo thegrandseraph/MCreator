cdfafd9995d25651c575dc0467c5b5ab2c737180
Domenick <Domenick@DOMENICKSLAPTOP>
2022-01-29 20:01:32 -0500

    MCreator Merge commit '399e3da441dc44f397dc193b1e30cb3bc0776e8b' into b147bd38a12106147534bc2a02355083dab63d61

--- src/main/java/net/mcreator/elementsoftantalus/init/ElementsOfTantalusModMenus.java
+++ src/main/java/net/mcreator/elementsoftantalus/init/ElementsOfTantalusModMenus.java
@@ -14,7 +14,6 @@ import net.minecraft.world.inventory.AbstractContainerMenu;
 
 import net.mcreator.elementsoftantalus.world.inventory.InscriptionTableGUIMenu;
 import net.mcreator.elementsoftantalus.world.inventory.InscriptionGUIMenu;
-import net.mcreator.elementsoftantalus.world.inventory.Inscriber1GUIMenu;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -22,8 +21,6 @@ import java.util.ArrayList;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ElementsOfTantalusModMenus {
 	private static final List<MenuType<?>> REGISTRY = new ArrayList<>();
-	public static final MenuType<Inscriber1GUIMenu> INSCRIBER_1_GUI = register("inscriber_1_gui",
-			(id, inv, extraData) -> new Inscriber1GUIMenu(id, inv, extraData));
 	public static final MenuType<InscriptionTableGUIMenu> INSCRIPTION_TABLE_GUI = register("inscription_table_gui",
 			(id, inv, extraData) -> new InscriptionTableGUIMenu(id, inv, extraData));
 	public static final MenuType<InscriptionGUIMenu> INSCRIPTION_GUI = register("inscription_gui",
