b416ff9beba12aff68e3435fec7ace56b9cbeec0
DomAwesome09 <Domenick@DOMENICKSLAPTOP>
2022-01-30 12:37:01 -0500

    still bugged, normal done

--- src/main/java/net/mcreator/elementsoftantalus/block/InscriptionTableBlock.java
+++ src/main/java/net/mcreator/elementsoftantalus/block/InscriptionTableBlock.java
@@ -4,6 +4,7 @@ package net.mcreator.elementsoftantalus.block;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.block.state.BlockState;
@@ -14,9 +15,14 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 import net.minecraft.client.Minecraft;
+
+import net.mcreator.elementsoftantalus.procedures.OnNormalRightClickProcedure;
 
 import java.util.Random;
 import java.util.List;
@@ -55,5 +61,20 @@ public class InscriptionTableBlock extends Block {
 			double z0 = z + 0.5 + (random.nextFloat() - 0.5) * 0.2D;
 			world.addParticle(ParticleTypes.ENCHANT, x0, y0, z0, 0, 0, 0);
 		}
+	}
+
+	@Override
+	public InteractionResult use(BlockState blockstate, Level world, BlockPos pos, Player entity, InteractionHand hand, BlockHitResult hit) {
+		super.use(blockstate, world, pos, entity, hand, hit);
+		int x = pos.getX();
+		int y = pos.getY();
+		int z = pos.getZ();
+		double hitX = hit.getLocation().x;
+		double hitY = hit.getLocation().y;
+		double hitZ = hit.getLocation().z;
+		Direction direction = hit.getDirection();
+
+		OnNormalRightClickProcedure.execute(world, x, y, z, entity);
+		return InteractionResult.SUCCESS;
 	}
 }
