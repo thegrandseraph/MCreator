8f787afa1e2dccb9a0c07f175e4b94531876a26e
DomAwesome09 <Domenick@DOMENICKSLAPTOP>
2022-01-30 18:38:42 -0500

    Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!  Sacrifice!

--- src/main/java/net/mcreator/elementsoftantalus/item/AstralHeart2Item.java
+++ src/main/java/net/mcreator/elementsoftantalus/item/AstralHeart2Item.java
@@ -1,10 +1,19 @@
 
 package net.mcreator.elementsoftantalus.item;
 
-import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.Component;
+
+import net.mcreator.elementsoftantalus.init.ElementsOfTantalusModTabs;
+
+import java.util.List;
 
 public class AstralHeart2Item extends Item {
-
 	public AstralHeart2Item() {
 		super(new Item.Properties().tab(ElementsOfTantalusModTabs.TAB_ELEMENTS_OF_TANTALUS).stacksTo(64).rarity(Rarity.COMMON));
 		setRegistryName("astral_heart_2");
@@ -25,5 +34,4 @@ public class AstralHeart2Item extends Item {
 		super.appendHoverText(itemstack, world, list, flag);
 		list.add(new TextComponent("The final power of Tantalus resides within."));
 	}
-
 }
--- src/main/java/net/mcreator/elementsoftantalus/item/KnifeOfSacrificeItem.java
+++ src/main/java/net/mcreator/elementsoftantalus/item/KnifeOfSacrificeItem.java
@@ -1,10 +1,23 @@
 
 package net.mcreator.elementsoftantalus.item;
 
-import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.Component;
+
+import net.mcreator.elementsoftantalus.procedures.SelfSacrifceProcedure;
+import net.mcreator.elementsoftantalus.init.ElementsOfTantalusModTabs;
+
+import java.util.List;
 
 public class KnifeOfSacrificeItem extends Item {
-
 	public KnifeOfSacrificeItem() {
 		super(new Item.Properties().tab(ElementsOfTantalusModTabs.TAB_ELEMENTS_OF_TANTALUS).stacksTo(64).rarity(Rarity.COMMON));
 		setRegistryName("knife_of_sacrifice");
@@ -12,7 +25,7 @@ public class KnifeOfSacrificeItem extends Item {
 
 	@Override
 	public int getUseDuration(ItemStack itemstack) {
-		return 0;
+		return 2;
 	}
 
 	@Override
@@ -22,4 +35,15 @@ public class KnifeOfSacrificeItem extends Item {
 		list.add(new TextComponent("Perhaps you can teach him."));
 	}
 
+	@Override
+	public InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand hand) {
+		InteractionResultHolder<ItemStack> ar = super.use(world, entity, hand);
+		ItemStack itemstack = ar.getObject();
+		double x = entity.getX();
+		double y = entity.getY();
+		double z = entity.getZ();
+
+		SelfSacrifceProcedure.execute(entity, itemstack);
+		return ar;
+	}
 }
