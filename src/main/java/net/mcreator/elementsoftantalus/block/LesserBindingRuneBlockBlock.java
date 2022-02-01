ecd91136a0808959617f4609d2feb33d00de04cc
DomAwesome09 <Domenick@DOMENICKSLAPTOP>
2022-01-31 20:05:06 -0500

    .

--- src/main/java/net/mcreator/elementsoftantalus/block/LesserBindingRuneBlockBlock.java
+++ src/main/java/net/mcreator/elementsoftantalus/block/LesserBindingRuneBlockBlock.java
@@ -1,15 +1,31 @@
 
 package net.mcreator.elementsoftantalus.block;
 
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.material.Material;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.item.TieredItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.core.BlockPos;
+import net.minecraft.client.Minecraft;
+
+import java.util.Random;
+import java.util.List;
+import java.util.Collections;
 
 public class LesserBindingRuneBlockBlock extends Block {
-
 	public LesserBindingRuneBlockBlock() {
 		super(BlockBehaviour.Properties.of(Material.WOOD).sound(SoundType.WOOD).strength(1f, 10f).requiresCorrectToolForDrops());
-
 		setRegistryName("lesser_binding_rune_block");
 	}
 
@@ -27,7 +43,6 @@ public class LesserBindingRuneBlockBlock extends Block {
 
 	@Override
 	public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
-
 		List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 		if (!dropsOriginal.isEmpty())
 			return dropsOriginal;
@@ -49,5 +64,4 @@ public class LesserBindingRuneBlockBlock extends Block {
 			world.addParticle(ParticleTypes.SOUL, x0, y0, z0, 0, 0, 0);
 		}
 	}
-
 }
